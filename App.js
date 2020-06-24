e3ac4e228db8c8fa04d34055ae1b77e32268d978
howtoosee <chenxihao0627@gmail.com>
2020-06-24 18:48:26 +0800

    resolve conflicts for merge

--- App.js
+++ App.js
@@ -1,81 +1,138 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, SafeAreaView} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-
-import Recommendation from './components/Recommendation';
-import Favourite from './components/Favourite';
-import StartScreen from './components/StartScreen';
-import Search from './components/Search';
-
-import Screen from './constants/Screen';
+import {
+    Favourite,
+    HelpBuyPls,
+    Home,
+    LogInScreen,
+    Profile,
+    Recommendation,
+    Search,
+    SearchResults,
+    SignUpScreen,
+    StartScreen
+} from './components';
 
 
 export default function App() {
 
-    const [currScreen, setCurrScreen] = useState(Screen.START_SCREEN);
-    const [searchHist, setSearchHist] = useState([]);
+// <<<<<<< HEAD
+//
+// export default function App() {
+//
+//     const [currScreen, setCurrScreen] = useState(Screen.START_SCREEN);
+//     const [searchHist, setSearchHist] = useState([]);
+//
+//     function showScreen(scrn) {
+//         let newScreen: any;
+//
+//         switch (scrn) {
+//             case "favourite":
+//                 newScreen = Screen.FAVOURITE;
+//                 break;
+//             case "recommendation":
+//                 newScreen = Screen.RECOMMENDATION;
+//                 break;
+//             case "search":
+//                 newScreen = Screen.SEARCH;
+//                 break;
+//             case "start_screen":
+//                 newScreen = Screen.START_SCREEN;
+//                 break;
+//             default:
+//                 newScreen = Screen.START_SCREEN;
+//                 break;
+//         }
+//         setCurrScreen(newScreen);
+//     }
+//
+//
+//     let content;
+//     switch (currScreen) {
+//         case Screen.FAVOURITE:
+//             content = <Favourite onPressBack={() => showScreen('start_screen')}/>;
+//             break;
+//         case Screen.RECOMMENDATION:
+//             content = <Recommendation onPressBack={() => showScreen('start_screen')}/>;
+//             break;
+//         case Screen.SEARCH:
+//             content = <Search onPressBack={() => showScreen('start_screen')}
+//                               searchHistory={searchHist}
+//                               setSearchHistory={setSearchHist}/>;
+//             break;
+//         case Screen.START_SCREEN:
+//             content = <StartScreen onPressRec={() => showScreen('recommendation')}
+//                                    onPressSearch={() => showScreen('search')}
+//                                    onPressFav={() => showScreen('favourite')}/>
+//             break;
+//         default:
+//             content = <StartScreen onPressRec={() => showScreen('recommendation')}
+//                                    onPressSearch={() => showScreen('search')}
+//                                    onPressFav={() => showScreen('favourite')}/>
+//             break;
+//
+//     }
+//
+// =======
 
-    function showScreen(scrn) {
-        let newScreen: any;
-
-        switch (scrn) {
-            case "favourite":
-                newScreen = Screen.FAVOURITE;
-                break;
-            case "recommendation":
-                newScreen = Screen.RECOMMENDATION;
-                break;
-            case "search":
-                newScreen = Screen.SEARCH;
-                break;
-            case "start_screen":
-                newScreen = Screen.START_SCREEN;
-                break;
-            default:
-                newScreen = Screen.START_SCREEN;
-                break;
+    const forFade = ({current}) => ({
+        cardStyle: {
+            opacity: current.progress
         }
-        setCurrScreen(newScreen);
-    }
+    });
 
-
-    let content;
-    switch (currScreen) {
-        case Screen.FAVOURITE:
-            content = <Favourite onPressBack={() => showScreen('start_screen')}/>;
-            break;
-        case Screen.RECOMMENDATION:
-            content = <Recommendation onPressBack={() => showScreen('start_screen')}/>;
-            break;
-        case Screen.SEARCH:
-            content = <Search onPressBack={() => showScreen('start_screen')}
-                              searchHistory={searchHist}
-                              setSearchHistory={setSearchHist}/>;
-            break;
-        case Screen.START_SCREEN:
-            content = <StartScreen onPressRec={() => showScreen('recommendation')}
-                                   onPressSearch={() => showScreen('search')}
-                                   onPressFav={() => showScreen('favourite')}/>
-            break;
-        default:
-            content = <StartScreen onPressRec={() => showScreen('recommendation')}
-                                   onPressSearch={() => showScreen('search')}
-                                   onPressFav={() => showScreen('favourite')}/>
-            break;
-
-    }
-
+    const Stack = createStackNavigator();
+// >>>>>>> main_logic
 
     return (
-        <View style={styles.screen}>
-            {content}
-        </View>
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Welcome"
+                                 mode={Platform.OS === 'ios' ? "modal" : "card"}>
+                    <Stack.Screen name="Welcome"
+                                  component={StartScreen}
+                    />
+                    <Stack.Screen name="Sign In"
+                                  component={LogInScreen}
+                                  options={{cardStyleInterpolator: forFade}}
+                    />
+                    <Stack.Screen name="Sign Up"
+                                  component={SignUpScreen}
+                                  options={{cardStyleInterpolator: forFade}}
+                    />
+                    <Stack.Screen name="Search"
+                                  component={Search}
+                                  options={{cardStyleInterpolator: forFade}}
+                    />
+                    <Stack.Screen name="SearchResults"
+                                  component={SearchResults}
+                                  options={{cardStyleInterpolator: forFade}}
+                    />
+                    <Stack.Screen name="Recommendation"
+                                  component={Recommendation}
+                                  options={{cardStyleInterpolator: forFade}}
+                    />
+                    <Stack.Screen name="Favourite"
+                                  component={Favourite}
+                                  options={{cardStyleInterpolator: forFade}}
+                    />
+                    <Stack.Screen name="Profile"
+                                  component={Profile}
+                                  options={{cardStyleInterpolator: forFade}}
+                    />
+                    <Stack.Screen name="Lemme Help"
+                                  component={HelpBuyPls}
+                                  options={{cardStyleInterpolator: forFade}}
+                    />
+                    <Stack.Screen name="Orbit Around Food"
+                                  component={Home}
+                                  options={{headerLeft: null}}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </SafeAreaProvider>
     );
 }
-
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1
-    }
-});
--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,92 +1,166 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, ScrollView, Modal, ActivityIndicator} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, ActivityIndicator} from 'react-native';
 
 import DefaultStyles from "../constants/DefaultStyles";
-import DefaultHeader from "./DefaultHeader";
 import Colors from "../constants/Colors";
 import searchQueryFood from "../api/SearchApi";
 
 
-function SearchResults(props) {
+function SearchResults({route, navigation}) {
+    const {searchKey} = route.params;
+
     const closeResults = () => {
-        props.onPress();
         setLoading(true);
         setResList([]);
+        navigation.goBack();
     }
 
     const [isLoading, setLoading] = useState(true);
     const [results, setResList] = useState([]);
 
-    if (props.showResults && results.length === 0 && isLoading) {
-        searchQueryFood(props.searchKey, setResList, setLoading);
-    }
 
-    const getKey = (name, objType) => name + '_' + objType + "_" + Math.floor(Math.random() * 10000);
+    if (isLoading) {
+        searchQueryFood(searchKey, setResList)
+            .then(() => setLoading(false))
+            .catch(err => console.log('Error querying:', err));
+    }
 
 
     return (
-        <Modal visible={props.showResults} animationType="slide">
 
-            <View style={DefaultStyles.screen}>
+        <View style={DefaultStyles.screen}>
 
-                <DefaultHeader headerText="Results" onPress={closeResults}/>
+            {/*<DefaultHeader headerText="Results" onPress={closeResults}/>*/}
 
-                <View style={DefaultStyles.contentContainer}>
-                    <View style={styles.searchResultInfo}><Text>Searching for: "{props.searchKey}"</Text></View>
-                    {(isLoading && (!props.showResults || results.length === 0))
-                        ? <ActivityIndicator/>
-                        : (results.length === 0)
-                            ? <View style={styles.searchResultInfo}><Text>No results found</Text></View>
-                            : <ScrollView style={styles.searchResults}>
-                                <Text>Results found: {results.length}</Text>
-                                {results.map(item => (
-                                    <View style={styles.searchResultContainer} key={getKey(item.name, 'list')}>
-                                        <Text style={styles.searchResultKey}>{item.name}</Text>
-                                        <Text style={styles.searchResultInfo}>{item.price}</Text>
-                                        <Text
-                                            style={styles.searchResultInfo}>{item.store.store_name} ({item.store.location})</Text>
-                                        <Text
-                                            style={styles.searchResultInfo}>{item.store.open_hours} - {item.store.close_hours}</Text>
-                                    </View>
-                                ))}
-                            </ScrollView>
+            <View style={DefaultStyles.contentContainer}>
 
-                    }
-
+                <View style={styles.resultStatInfo}>
+                    <Text>Searching for: "{searchKey}"</Text>
                 </View>
 
-            </View>
+                {(isLoading)
 
-        </Modal>
+                    ? <View style={styles.loadingIndicatorContainer}>
+                        <ActivityIndicator size='large' color='black'/>
+                    </View>
+
+                    : (results.length === 0)
+
+                        ? <View style={styles.noResultsContainer}>
+                            <View style={styles.searchResultInfo}>
+                                <Text>No results found :(</Text>
+                            </View>
+                        </View>
+
+                        : <View>
+                            <View style={styles.resultStatInfo}>
+                                <Text>Results found: {results.length}</Text>
+                            </View>
+
+
+                            <ScrollView style={styles.searchResults}>
+                                {
+                                    results.map(item => getResultItemElement(item))
+                                }
+
+                                <View style={styles.endOfResultsText}>
+                                    <Text>No more liao!</Text>
+                                </View>
+
+                            </ScrollView>
+
+                        </View>
+                }
+
+            </View>
+        </View>
+
     )
 }
+
+
+function getResultItemElement(item) {
+    const getKey = (name, objType) => name + '_' + objType + "_" + Math.floor(Math.random() * 10000);
+
+    return (
+        <View style={styles.searchResultContainer} key={getKey(item.name, 'list')}>
+
+            <Text style={styles.searchResultKey}>
+                {item.name}
+            </Text>
+
+            <Text style={styles.searchResultInfo}>
+                {item.price}
+            </Text>
+
+            <Text style={styles.searchResultInfo}>
+                {item.store.store_name} ({item.store.location})
+            </Text>
+
+            <Text style={styles.searchResultInfo}>
+                {item.store.open_hours} - {item.store.close_hours}
+            </Text>
+
+        </View>
+    );
+}
+
 
 const styles = StyleSheet.create({
     searchResults: {
         marginLeft: 0,
-        paddingTop: 10,
+        paddingTop: 0,
     },
 
     searchResultContainer: {
-        marginTop: 15,
+        marginTop: 10,
         padding: 10,
         borderWidth: 2,
-        borderColor: Colors.CARD,
-        borderRadius: 5,
+        // borderColor: Colors.CARD,
+        borderColor: Colors.TEXT,
+        borderRadius: 4,
         width: '97%',
         color: Colors.TEXT,
     },
 
     searchResultKey: {
         color: Colors.TEXT,
-        fontSize: 18,
+        fontSize: 16,
         fontWeight: "bold",
     },
 
     searchResultInfo: {
         color: Colors.TEXT,
-        fontSize: 16,
+        fontSize: 14,
     },
+
+    loadingIndicatorContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 100,
+    },
+
+    noResultsContainer: {
+        paddingTop: 250,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    resultStatInfo: {
+        marginTop: 4,
+        marginBottom: 4,
+        fontSize: 14,
+        color: Colors.TEXT,
+        fontStyle: 'italic',
+    },
+
+    endOfResultsText: {
+        height: 180,
+        paddingVertical: 20,
+        fontStyle: 'italic',
+        alignItems: 'center',
+        // justifyContent: 'center'
+    }
 });
 
 

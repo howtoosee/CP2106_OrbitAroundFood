c64175f8b91696da98c3382b749887f20352ac1b
howtoosee <chenxihao0627@gmail.com>
2020-07-14 05:51:41 +0800

    add files to index

--- components/index.js
+++ components/index.js
@@ -1,31 +1,38 @@
+import DefaultHeader from "./DefaultHeader";
 import Favourite from './Favourite';
 import Filter from './Filter';
 import FoodDetails from './FoodDetails';
 import HelpBuyPls from './HelpBuyPls';
-import Home from './Home';
-import LemmeHelpContainer from './LemmeHelpContainer';
 import LeaveReview from './LeaveReview';
-import LogInScreen from './LogInScreen';
+import LemmeHelpContainer from './LemmeHelpContainer';
+import NoSignInWarningDialogue from './NoSignInWarningDialogue';
 import Profile from './Profile';
 import Recommendation from './Recommendation';
 import Search from './Search';
 import SearchResults from './SearchResults';
+import SignInScreen from './SignInScreen';
 import SignUpScreen from './SignUpScreen';
 import StartScreen from './StartScreen';
+import StartScreenButton from './StartScreenButton';
+import StartScreenLoggedIn from './StartScreenLoggedIn';
+
 
 export {
+    DefaultHeader,
     Favourite,
     Filter,
     FoodDetails,
     HelpBuyPls,
-    Home,
-    LemmeHelpContainer,
     LeaveReview,
-    LogInScreen,
+    LemmeHelpContainer,
+    NoSignInWarningDialogue,
     Profile,
     Recommendation,
     Search,
     SearchResults,
+    SignInScreen,
     SignUpScreen,
-    StartScreen
+    StartScreen,
+    StartScreenButton,
+    StartScreenLoggedIn
 };

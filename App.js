6fc52a97e40a1e01234f9f18a35a66f7172158f9
howtoosee <chenxihao0627@gmail.com>
2020-07-18 21:23:00 +0800

    update transition for android

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import getSlideFromRightTransition from 'react-navigation-slide-from-right-transition';
 
 import {
     Favourite,
@@ -18,20 +19,10 @@ import {
     StartScreen
 } from './components';
 
-import {Platform} from "react-native";
-
 
 export default function App() {
 
-
-    const forFade = ({current}) => ({
-        cardStyle: {
-            opacity: current.progress
-        }
-    });
-
     const stack = createStackNavigator();
-
 
     return (
         <SafeAreaProvider>
@@ -39,7 +30,9 @@ export default function App() {
             <NavigationContainer>
 
                 <stack.Navigator initialRouteName="Welcome"
-                                 mode={Platform.OS === 'ios' ? "modal" : "card"}>
+                                 mode={'card'}
+                                 transitionConfig={getSlideFromRightTransition}
+                >
 
                     <stack.Screen name="Welcome"
                                   component={StartScreen}
@@ -52,53 +45,45 @@ export default function App() {
 
                     <stack.Screen name="Sign In"
                                   component={SignInScreen}
-                                  options={{cardStyleInterpolator: forFade}}
                     />
 
                     <stack.Screen name="Sign Up"
                                   component={SignUpScreen}
-                                  options={{cardStyleInterpolator: forFade}}
                     />
 
                     <stack.Screen name="Search"
                                   component={Search}
-                                  options={{cardStyleInterpolator: forFade}}
                     />
 
                     <stack.Screen name="Search Results"
                                   component={SearchResults}
-                                  options={{cardStyleInterpolator: forFade}}
                     />
 
                     <stack.Screen name="Food Details"
                                   component={FoodDetails}
-                                  options={{cardStyleInterpolator: forFade}}
                     />
 
                     <stack.Screen name="Leave Review"
                                   component={LeaveReview}
-                                  options={{cardStyleInterpolator: forFade}}
                     />
 
                     <stack.Screen name="Recommendation"
                                   component={Recommendation}
-                                  options={{cardStyleInterpolator: forFade}}
                     />
 
                     <stack.Screen name="Favourite"
                                   component={Favourite}
-                                  options={{cardStyleInterpolator: forFade}}
                     />
 
 
                     <stack.Screen name="Lemme Help"
                                   component={HelpBuyPls}
-                                  options={{cardStyleInterpolator: forFade}}
+                                  // options={{cardStyleInterpolator: forFade}}
                     />
 
                     <stack.Screen name="Profile"
                                   component={Profile}
-                                  options={{cardStyleInterpolator: forFade}}
+                                  // options={{cardStyleInterpolator: forFade}}
                     />
 
                 </stack.Navigator>
